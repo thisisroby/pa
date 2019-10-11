5aada4f19a605b7289a11aac7f783c64d8053b94
Jaak-Robert <robert.maesalu@gmail.com>
2019-10-11 10:38:34 +0300

    Sõjaväe kohustuse arvutamine

--- tingimuslaused/taringuvise.php
+++ tingimuslaused/taringuvise.php
@@ -1,9 +1,28 @@
 <?php
-//defineerime vajalikud muutujad
-$number = rand(1, 6);
+// defineerime vajalikud muutujad
+$number = rand(1, 7);
 // switch kontrollimiseks
-switch ($number) {
+switch ($number){
     case 1:
-        echo 'Sinu number: '.$number.'<br>';
+        echo 'Sinu number 1<br>';
+        break;
+    case 2:
+        echo 'Sinu number 2<br>';
+        break;
+    case 3:
+        echo 'Sinu number 3<br>';
+        break;
+    case 4:
+        echo 'Sinu number 4<br>';
+        break;
+    case 5:
+        echo 'Sinu number 5<br>';
+        break;
+    case 6:
+        echo 'Sinu number 6<br>';
+        break;
+    default:
+        echo 'Midagi läks valesti<br>';
         break;
 }
+echo 'Sellega täringuvise on tehtud';

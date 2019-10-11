fa7ffbb0a68dd2ede025373300e94ec61c6cd0d2
Jaak-Robert <robert.maesalu@gmail.com>
2019-10-11 11:43:06 +0300

    Read, veerud

---
+++ tsüklid/pi.php
@@ -0,0 +1,16 @@
+<?php
+$kord = 1;
+$kordMax = 1000;
+$summa = 0;
+while($kord <= $kordMax) {
+    $element = 1 / (2 * $kord - 1);
+    $jaak = $kord % 2;
+    if($jaak == 0) {
+        $summa = $summa - $element;
+    } else {
+        $summa = $summa + $element;
+    }
+    $kord++;
+}
+$pi = $summa * 4;
+echo $pi;
--- tsüklid/tarniruut.php
+++ tsüklid/tarniruut.php
@@ -1,9 +1,13 @@
 <?php
+//ridade ja veergude max
+$ridadeArv = 7;
+$veergudeArv = 7;
 // ridade maaramine
-for($reaNr = 1; $reaNr < 5; $reaNr++){
+for($reaNr = 1; $reaNr <= 5; $reaNr++){
     echo $reaNr;
     // veergude maaramine ja vahetus
     for($veeruNr = 1; $veeruNr < 4; $veeruNr++){
         echo '*';
     }
+    echo '<br>';
 }

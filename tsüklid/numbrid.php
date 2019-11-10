35fa79e697e39a319913ae0c7800032b747a777e
Jaak-Robert <robert.maesalu@gmail.com>
2019-11-10 12:02:58 +0200

    Numbrid

---
+++ tsüklid/numbrid.php
@@ -0,0 +1,17 @@
+<?php
+//ridade ja veergude maksimum
+$ridadeArv = 7;
+$veergudeArv = 7;
+//ridade määramine ja vahetus
+for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++) {
+    echo $reaNumber;
+    //veergude määramine ja vahetus
+    for ($veeruNumber = 1; $veeruNumber <= $reaNumber; $veeruNumber++)
+        if ($veeruNumber != $reaNumber) {
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+        } else {
+            echo '*';
+        }
+    echo '<br>';
+}
+?>

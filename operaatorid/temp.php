1a3fc20db19a6e421f245f378fde41f064053b81
Jaak-Robert <robert.maesalu@gmail.com>
2019-11-10 12:16:19 +0200

    Temperatuuri teisendamine

---
+++ operaatorid/temp.php
@@ -0,0 +1,8 @@
+<?php
+// muutujad
+// väärtused
+$celsius = 15;
+$fahrenheit = (9 / 5) * $celsius + 32;
+// tulemus täpsusega 2 kohta pärast koma
+printf('Temperatuur: %d&deg;C = %.2f&deg;F', $celsius, $fahrenheit);
+?>
